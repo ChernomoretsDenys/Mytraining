0324cf608c56ebf16eee0a43b4289ab2a7f6ca1e
Denys Chornomorets <cornomorecdenis@gmail.com>
2024-04-18 17:31:33 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict"
+
+fetch('./trainingData.json')
+	.then((response) => response.json())
+	.then((json) => console.log(json))
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, remove, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const databaseLink = {
+	databaseURL: "https://training-7c03e-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const databaseName = "training"
+
+const initApp = initializeApp(databaseLink)
+const database = getDatabase(initApp)
+const reference = ref(database, databaseName)
+
+
+
+const container = document.getElementById("container")
+
