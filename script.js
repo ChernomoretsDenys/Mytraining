7f216c0776f67289128682e2f4a155210c5800ed
Denys Chornomorets <cornomorecdenis@gmail.com>
2024-04-26 21:32:43 +0300

    First refurbish

--- script.js
+++ script.js
@@ -1,34 +1,70 @@
-"use strict"
+"use strict";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const databaseLink = {
 	databaseURL: "https://training-7c03e-default-rtdb.europe-west1.firebasedatabase.app/"
 }
-const databaseName = "training"
-const initApp = initializeApp(databaseLink)
-const database = getDatabase(initApp)
-const reference = ref(database, databaseName)
 
-let weekArray = null
+// let name = null;
+
+// while (name !== "ann" && name !== "den") {
+// 	name = prompt("Enter your name:").toLowerCase();
+// }
+
+// let databaseName = null;
+const wellcomeBack = document.getElementById("container_welcomeBack");
+wellcomeBack.textContent = "Wellcome back Den!"
+
+// if (name === "den") {
+// 	databaseName = "trainingDen";
+// 	wellcomeBack.textContent = "Wellcome back Den!"
+// } else if (name === "ann") {
+// 	databaseName = "trainingAnn";
+// 	wellcomeBack.textContent = "Wellcome back Ann!"
+// }
+
+const initApp = initializeApp(databaseLink);
+const database = getDatabase(initApp);
+const reference = ref(database, "trainingDen");
+
+let weekArray = null;
+let weekArrayLength = null;
+
 onValue(reference, function (snapshot) {
 	if (snapshot.exists()) {
-		weekArray = Object.entries(snapshot.val())
-		checkCurrentTime()
+		weekArray = Object.entries(snapshot.val());
+		weekArrayLength = weekArray.length;
+		checkCurrentTime();
 	} else {
-		alert("Something went wrong")
+		alert("Something went wrong");
 	}
 })
 
+const optionsButton = document.getElementById("optionsButton");
+const navigationOpt = document.querySelector(".navigation_options");
+
+const changeAccount = document.getElementById("change-account");
+const selectSpecificDay = document.getElementById("select-specifDay");
+const showProgress = document.getElementById("show-progress");
+const viewOthers = document.getElementById("view-others");
+
+optionsButton.addEventListener("click", function () {
+	navigationOpt.classList.toggle("open");
+	const buttonText = optionsButton.textContent;
+	optionsButton.textContent = buttonText === "Options" ? "Exit" : "Options";
+});
+
+
 function checkCurrentTime() {
-	const date = new Date()
-	const currentDayOfWeek = date.getDay()
-	let askWeek = null
+	const date = new Date();
+	const currentDayOfWeek = date.getDay();
+	let askWeek = null;
 	while (askWeek == null || askWeek > 3) {
-		askWeek = Number(prompt("Current week:", 1))
+		askWeek = Number(prompt("Current week:", 1));
 	}
-	eachDay(currentDayOfWeek, askWeek)
+	eachDay(currentDayOfWeek, askWeek);
 }
 
 const container = document.getElementById("container_table")
@@ -80,9 +116,9 @@ function displayWeek(weekDay) {
 		} else {
 			newEl = document.createElement('td')
 			if (i < 8) {
-				newEl.style.backgroundColor = "green"
+				newEl.style.backgroundColor = "#08f"
 			} else {
-				newEl.style.backgroundColor = "red"
+				newEl.style.backgroundColor = "#f03"
 			}
 		}
 
